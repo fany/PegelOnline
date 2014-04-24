484804c1b1ff24f7a815b76be1bcb08b0ab21162
Martin H. Sluka <martin@sluka.de>
2014-04-24 17:04:23 +0200

    Lade waters.json

--- app/controller/Waters.js
+++ app/controller/Waters.js
@@ -1,6 +1,8 @@
 Ext.define('PegelOnline.controller.Waters', {
     extend: 'Ext.app.Controller',
 
+    requires: [ 'PegelOnline.view.Main' ],
+
     config : {
         control : {
             waters : {
