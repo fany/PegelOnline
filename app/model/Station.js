8dd48b10a882e47bdbadd79bef9d25df331fe728
Martin H. Sluka <martin@sluka.de>
2014-04-25 15:16:56 +0200

    .

--- app/model/Station.js
+++ app/model/Station.js
@@ -3,15 +3,15 @@ Ext.define('PegelOnline.model.Station', {
 
     config: {
         fields: [
-            { name: 'uuid', type: 'string' },
-            { name: 'number', type: 'int' },
+            { name: 'uuid',      type: 'string' },
+            { name: 'number',    type: 'int'    },
             { name: 'shortname', type: 'string' },
-            { name: 'longname', type: 'string' },
-            { name: 'km', type: 'float' },
-            { name: 'agency', type: 'string' },
-            { name: 'longitude', type: 'float' },
-            { name: 'latitude', type: 'float' },
-            { name: 'water', type: 'auto' }
+            { name: 'longname',  type: 'string' },
+            { name: 'km',        type: 'float'  },
+            { name: 'agency',    type: 'string' },
+            { name: 'longitude', type: 'float'  },
+            { name: 'latitude',  type: 'float'  },
+            { name: 'water',     type: 'auto'   }
         ]
     }
 });
--- app/model/Water.js
+++ app/model/Water.js
@@ -1,10 +1,10 @@
 Ext.define('PegelOnline.model.Water', {
-    extend: 'Ext.data.Model',
+    extend : 'Ext.data.Model',
 
     config: {
         fields: [
             { name: 'shortname', type: 'string' },
-            { name: 'longname', type: 'string' }
+            { name: 'longname',  type: 'string' }
         ]
     }
 });
